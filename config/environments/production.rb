ccf17a2a51dd36b4c4c3826a0029afa5e5511081
MattFlaig <matt.flaig@web.de>
2014-05-09 18:16:37 +0200

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,18 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         =>  'http://vast-oasis-4643.herokuapp.com/',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
+
+
 end
