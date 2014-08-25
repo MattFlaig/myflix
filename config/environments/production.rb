fef0644376321eb9fe10e0a6e3d523b945144d15
Matthias Flaig <matt.flaig@web.de>
2014-08-25 12:48:35 +0200

    changed mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,21 +6,17 @@ Myflix::Application.configure do
   config.action_controller.perform_caching = true
 
   config.serve_static_assets = true
-
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
-
   config.assets.compile = true
   config.assets.digest = true
 
   config.i18n.fallbacks = true
-
   config.active_support.deprecation = :notify
 
-  Rails.application.routes.default_url_options[:host] = '???'
-  
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'http://quiet-chamber-2363.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
