65eaf198ab5df09ed3c4438a6abf864bb698f4b7
Matthias Flaig <matt.flaig@web.de>
2014-11-06 22:37:16 +0100

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Myflix
     config.active_support.escape_html_entities_in_json = true
 
     config.assets.enabled = true
+    config.assets.version = 1.0
     config.generators do |g|
       g.orm :active_record
       g.template_engine :haml
