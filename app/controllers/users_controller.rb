2d0bfba89d6b07ae41793cd19757c8c5c0a4d53e
Matthias Flaig <matt.flaig@web.de>
2015-01-27 21:39:13 +0100

    strong params issue in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :full_name)
+    params.require(:user).permit(:email, :password, :full_name, :stripeToken, :invitation_token)
   end
 
 end
